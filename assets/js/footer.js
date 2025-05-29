a625f8e0b142c5e76d9b7bc5db13ec7282ac67ba
Franknew2 <francescoamerio95@gmail.com>
2025-05-29 09:45:34 +0200

    formattato il codice

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,10 +2,11 @@
 // Inizializza la mappa centrata sull’Italia
 var map = L.map('map').setView([43.5, 11.5], 4.5); // Centro Italia, zoom adatto
 
-// Aggiunge le tile di OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
+// Markers
 
 // Torino - Sede 1
 L.marker([45.0703, 7.6869]).addTo(map)
@@ -34,3 +35,8 @@ L.marker([43.7696, 11.2558]).addTo(map)
 // Perugia
 L.marker([43.1107, 12.3908]).addTo(map)
   .bindPopup('Revo-bike Perugia');
+
+// Gallipoli
+L.marker([40.0555, 17.9973]).addTo(map)
+  .bindPopup('Revo-bike Gallipoli');
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-let elements = document.querySelectorAll(".nav");
+  let elements = document.querySelectorAll(".nav");
 
-const step1 = document.getElementById("step-1")
-const step2 = document.getElementById("step-2")
-const step3 = document.getElementById("step-3")
-const step4 = document.getElementById("step-4")
-const step5 = document.getElementById("step-5")
+  const step1 = document.getElementById("step-1")
+  const step2 = document.getElementById("step-2")
+  const step3 = document.getElementById("step-3")
+  const step4 = document.getElementById("step-4")
+  const step5 = document.getElementById("step-5")
 
-elements.forEach(element => {
-  element.onmouseover = function() {
-    this.style.color = "red";
-  };
-  element.onmouseout = function() {
-    this.style.color = "aliceblue";
-  };
-});
+  elements.forEach(element => {
+    element.onmouseover = function () {
+      this.style.color = "red";
+    };
+    element.onmouseout = function () {
+      this.style.color = "aliceblue";
+    };
+  });
 
-function changeVisibility(hide, show){
-  hide.style.display = 'none'
-  show.style.display = 'flex'
-}
+  function changeVisibility(hide, show) {
+    hide.style.display = 'none'
+    show.style.display = 'flex'
+  }
 
-document.getElementById('btn-start').addEventListener('click', function() {
-  changeVisibility(step1,step2)
-});
+  document.getElementById('btn-start').addEventListener('click', function () {
+    changeVisibility(step1, step2)
+  });
 
-document.getElementById('btn-offroad').addEventListener('click', function() {
-  changeVisibility(step2,step3)
-});
+  document.getElementById('btn-offroad').addEventListener('click', function () {
+    changeVisibility(step2, step3)
+  });
 
-document.getElementById('btn-muscolare').addEventListener('click', function() {
-  changeVisibility(step3,step4)
-});
+  document.getElementById('btn-muscolare').addEventListener('click', function () {
+    changeVisibility(step3, step4)
+  });
 
-document.getElementById('btn-strada').addEventListener('click', function() {
-  changeVisibility(step2,step5)
-});
+  document.getElementById('btn-strada').addEventListener('click', function () {
+    changeVisibility(step2, step5)
+  });
 
-document.getElementById('btn-citta').addEventListener('click', function() {
-   window.location.href = 'city.html';
-});
+  document.getElementById('btn-citta').addEventListener('click', function () {
+    window.location.href = 'city.html';
+  });
 
-document.getElementById('btn-viaggio').addEventListener('click', function() {
-  window.location.href = 'city.html#trekking';
-});
+  document.getElementById('btn-viaggio').addEventListener('click', function () {
+    window.location.href = 'city.html#trekking';
+  });
 
-document.getElementById('btn-elettrica').addEventListener('click', function() {
-  window.location.href = 'ebikes.html';
-});
+  document.getElementById('btn-elettrica').addEventListener('click', function () {
+    window.location.href = 'ebikes.html';
+  });
 
-document.getElementById('btn-discesa').addEventListener('click', function() {
-  window.location.href = 'mountain.html';
-});
+  document.getElementById('btn-discesa').addEventListener('click', function () {
+    window.location.href = 'mountain.html';
+  });
 
-document.getElementById('btn-pedalare').addEventListener('click', function() {
-  window.location.href = 'mountain.html#trail';
-});
+  document.getElementById('btn-pedalare').addEventListener('click', function () {
+    window.location.href = 'mountain.html#trail';
+  });
 
 
 });
